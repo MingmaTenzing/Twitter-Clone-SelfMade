c195506d450ef8920b2a94466133fe1056b3e569
Tenzing Sherpa <pasangsherpa689@gmail.com>
2023-12-07 12:58:09 +0800

    login sign up page overhaul complete but needs realworld testing

--- src/pages/createprofile/index.tsx
+++ src/pages/createprofile/index.tsx
@@ -2,8 +2,11 @@ import { auth } from "@/firebase/init";
 import { onAuthStateChanged, updateProfile } from "firebase/auth";
 import { useRouter } from "next/router";
 import { FormEvent, useEffect, useState } from "react";
-import twitter from "../../../assests/twitter.png";
 import Image from "next/image";
+import x from "../../../assests/X_logo_2023_(white).png";
+import Google from "../../../assests/google.png";
+import Apple from "../../../assests/apple.png";
+import { TextField } from "@mui/material";
 
 export interface user {
   displayName: string;
@@ -15,9 +18,9 @@ export interface user {
 type Props = {};
 function CreateProfile({}: Props) {
   const router = useRouter();
+  const [error, setError] = useState<boolean>(false);
   const [userName, setuserName] = useState<string>();
   const [photoURL, setPhotoURL] = useState<string>();
-
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -34,57 +37,70 @@ function CreateProfile({}: Props) {
       photoURL: photoURL,
     })
       .then(() => {
-        router.push("/")
+        router.push("/");
       })
       .catch((error) => {
+        console.log(error);
       });
   }
 
   return (
-    <div className="p-4  md:w-[500px] mt-10 m-auto">
-      <div className="flex  flex-col items-center space-y-4">
-        <Image
-          src={twitter}
-          alt="twitter logo"
-          width={200}
-          height={200}
-          className="w-20"
-        />
+    <section className=" p-8 bg-black w-full  h-[100vh] md:overflow-hidden m:auto text-white ">
+      <div className="   bg-black  text-white  p-6  md:w-1/2 md:m-auto lg:w-1/3 flex-col items-center  h-full ">
+        <div className="  space-y-8 flex flex-col justify-center h-full ">
+          <div className=" flex  justify-center">
+            <Image src={x} alt="x logo" className="w-8 " />
+          </div>
+          <p className=" font-bold text-2xl"> Create your Profile</p>
 
-        <h1 className="text-2xl font-bold ">Create your Profile</h1>
-      </div>
-      <form onSubmit={addUserDetails} className="mt-10">
-        <h2 className="font-bold text-lg">Profile Details</h2>
-        <div className="mt-4 space-y-4">
-          <div className="">
-            <input
-              type="text"
+          <form
+            className=" flex flex-col justify-center space-y-6"
+            onSubmit={addUserDetails}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Name"
               required
-              className="border p-3 outline-none w-full"
-              placeholder="Name"
+              type="text"
+              variant="outlined"
+              sx={{
+                fieldset: { borderColor: "#242424" },
+                input: { color: "white" },
+                label: { color: "#242424" },
+                borderColor: "white",
+                border: { color: "white" },
+              }}
               onChange={(e) => setuserName(e.target.value)}
-            ></input>
-          </div>
-          <div className="">
-            <input
-              minLength={8}
-              
-              type="url"
-              className="border p-3 outline-none w-full"
-              placeholder="PROFILE PHOTO URL (OPTIONAL)"
+            />
+            <TextField
+              id="outlined-basic"
+              label="Profile Picture Link"
+              required
+              variant="outlined"
+              sx={{
+                fieldset: { borderColor: "#242424" },
+                input: { color: "white" },
+                label: { color: "#242424" },
+                borderColor: "white",
+                border: { color: "white" },
+              }}
+              error={error}
+              helperText="The Image Link is too long. Make it short"
               onChange={(e) => setPhotoURL(e.target.value)}
-            ></input>
-          </div>
+            />
+            <div>
+              <button
+                type="submit"
+                className=" w-full h-12 font-semibold rounded-full text-white bg-twitter"
+              >
+                {" "}
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-
-        <button
-          type="submit"
-          className="text-white  mt-4 bg-black px-4 py-2 rounded-full"
-        >
-          Sign Up
-        </button>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 }
 export default CreateProfile;
